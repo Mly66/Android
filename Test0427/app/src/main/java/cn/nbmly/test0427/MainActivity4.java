dd06be822ad0ec997b6399bdcf35e4f193e8995a
Mly66 <3127328609@qq.com>
2025-04-27 15:39:23 +0800

    password&login

--- Test0427/app/src/main/java/cn/nbmly/test0427/MainActivity4.java
+++ Test0427/app/src/main/java/cn/nbmly/test0427/MainActivity4.java
@@ -1,5 +1,7 @@
 package cn.nbmly.test0427;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,7 +18,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity4 extends AppCompatActivity {
-    private Button btnConfirm, btnList, btnSingleChoice, btnMultiChoice, btnInput;
+    private Button btnConfirm, btnList, btnSingleChoice, btnMultiChoice, btnInput, btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +35,14 @@ public class MainActivity4 extends AppCompatActivity {
         btnSingleChoice = findViewById(R.id.btn_single_choice);
         btnMultiChoice = findViewById(R.id.btn_multi_choice);
         btnInput = findViewById(R.id.btn_input);
+        btnLogin = findViewById(R.id.btn_login);
+
         btnConfirm.setOnClickListener(v -> showConfirmDialog());
         btnList.setOnClickListener(v -> showListDialog());
         btnSingleChoice.setOnClickListener(v -> showSingleChoiceDialog());
         btnMultiChoice.setOnClickListener(v -> showMultiChoiceDialog());
         btnInput.setOnClickListener(v -> showInputDialog());
+        btnLogin.setOnClickListener(v -> showLoginDialog());
 
 
     }
@@ -56,26 +61,24 @@ public class MainActivity4 extends AppCompatActivity {
 
     public void showListDialog() {
         new AlertDialog.Builder(this)
-                .setTitle("选择一个选项")
+                .setTitle("选择你所在的城市")
                 .setIcon(android.R.drawable.ic_dialog_info)
-                .setPositiveButton("确定", (dialog, which) -> {
-                    Toast.makeText(this, "点击了确定", Toast.LENGTH_SHORT).show();
-                    //finish();
-
-                })
-                .setItems(new String[]{"选项1", "选项2", "选项3"}, (dialog, which) -> {
+                .setItems(new String[]{"北京", "上海", "广州"}, (dialog, which) -> {
                     switch (which) {
                         case 0:
-                            Toast.makeText(this, "您点击了：选项1", Toast.LENGTH_SHORT).show();
-                            Log.i("MainActivity4", "您点击了：选项1");
+                            btnList.setText("北京");
+                            Toast.makeText(this, "您点击了：北京", Toast.LENGTH_SHORT).show();
+                            Log.i("MainActivity4", "您点击了：北京");
                             break;
                         case 1:
-                            Toast.makeText(this, "您点击了：选项2", Toast.LENGTH_SHORT).show();
-                            Log.i("MainActivity4", "您点击了：选项2");
+                            btnList.setText("上海");
+                            Toast.makeText(this, "您点击了：上海", Toast.LENGTH_SHORT).show();
+                            Log.i("MainActivity4", "您点击了：上海");
                             break;
                         case 2:
-                            Toast.makeText(this, "您点击了：选项3", Toast.LENGTH_SHORT).show();
-                            Log.i("MainActivity4", "您点击了：选项3");
+                            btnList.setText("广州");
+                            Toast.makeText(this, "您点击了：广州", Toast.LENGTH_SHORT).show();
+                            Log.i("MainActivity4", "您点击了：广州");
                             break;
                         default:
                             Toast.makeText(this, "未知选项：" + which, Toast.LENGTH_SHORT).show();
@@ -96,19 +99,26 @@ public class MainActivity4 extends AppCompatActivity {
                     //finish();
 
                 })
-                .setSingleChoiceItems(new String[]{"选项1", "选项2", "选项3"}, 0, (dialog, which) -> {
+                .setSingleChoiceItems(new String[]{"红色", "绿色", "蓝色"}, 0, (dialog, which) -> {
                     switch (which) {
                         case 0:
-                            Toast.makeText(this, "您点击了：选项1", Toast.LENGTH_SHORT).show();
-                            Log.i("MainActivity4", "您点击了：选项1");
+                            btnSingleChoice.setBackgroundColor(Color.RED);
+                            Toast.makeText(this, "您点击了：红色", Toast.LENGTH_SHORT).show();
+                            Log.i("MainActivity4", "您点击了：红色");
                             break;
                         case 1:
-                            Toast.makeText(this, "您点击了：选项2", Toast.LENGTH_SHORT).show();
-                            Log.i("MainActivity4", "您点击了：选项2");
+                            btnSingleChoice.setBackgroundColor(Color.GREEN);
+                            Toast.makeText(this, "您点击了：绿色", Toast.LENGTH_SHORT).show();
+                            Log.i("MainActivity4", "您点击了：绿色");
                             break;
                         case 2:
-                            Toast.makeText(this, "您点击了：选项3", Toast.LENGTH_SHORT).show();
-                            Log.i("MainActivity4", "您点击了：选项3");
+                            btnSingleChoice.setBackgroundColor(Color.BLUE);
+                            Toast.makeText(this, "您点击了：蓝色", Toast.LENGTH_SHORT).show();
+                            Log.i("MainActivity4", "您点击了：蓝色");
+                            break;
+                        default:
+                            Toast.makeText(this, "未知选项：" + which, Toast.LENGTH_SHORT).show();
+                            Log.w("MainActivity4", "未知选项：" + which);
                             break;
                     }
                 })
@@ -117,27 +127,31 @@ public class MainActivity4 extends AppCompatActivity {
     }
 
     public void showMultiChoiceDialog() {
-        boolean[] checkedItems = new boolean[]{false, false, false};
+        String[] subjects = new String[]{"物理", "化学", "生物", "历史", "政治", "地理"};
+        boolean[] checkedItems = new boolean[subjects.length];
+
         new AlertDialog.Builder(this)
-                .setTitle("选择多个选项")
+                .setTitle("选择你高中选科")
                 .setIcon(android.R.drawable.ic_dialog_info)
-                .setMultiChoiceItems(new String[]{"选项1", "选项2", "选项3"}, checkedItems, (dialog, which, isChecked) -> {
+                .setMultiChoiceItems(subjects, checkedItems, (dialog, which, isChecked) -> {
                     checkedItems[which] = isChecked;
                 })
                 .setPositiveButton("确定", (dialog, which) -> {
-                    StringBuilder selectedItems = new StringBuilder();
-                    for (int i = 0; i < checkedItems.length; i++) {
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < subjects.length; i++) {
                         if (checkedItems[i]) {
-                            selectedItems.append(i + 1).append(" ");
+                            sb.append(subjects[i]).append(" ");
                         }
                     }
-                    Toast.makeText(this, "您选择了：" + selectedItems.toString(), Toast.LENGTH_SHORT).show();
-                    Log.i("MainActivity4", "您选择了：" + selectedItems.toString());
+                    String result = sb.toString().trim();
+                    btnMultiChoice.setText(result);
+                    Toast.makeText(this, "您选择了：" + result, Toast.LENGTH_SHORT).show();
+                    Log.i("MainActivity4", "您选择了：" + result);
                 })
                 .setNegativeButton("取消", null)
                 .show();
-
     }
+
 
     public void showInputDialog() {
         View dialogView = LayoutInflater.from(this)
@@ -152,11 +166,43 @@ public class MainActivity4 extends AppCompatActivity {
                     String inputText = editText.getText().toString();
                     Toast.makeText(this, "您输入了：" + inputText, Toast.LENGTH_SHORT).show();
                     Log.i("MainActivity4", "您输入了：" + inputText);
+                    btnInput.setText(inputText);
                 })
                 .setNegativeButton("取消", null)
                 .show();
     }
 
+    public void showLoginDialog() {
+        View dialogView = LayoutInflater.from(this)
+                .inflate(R.layout.dialog_login, null);
+        EditText etUsername = dialogView.findViewById(R.id.etUsername);
+        EditText etPassword = dialogView.findViewById(R.id.etPassword);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button Login = dialogView.findViewById(R.id.Login);
+        Login.setOnClickListener(v -> {
+            String username = etUsername.getText().toString();
+            String password = etPassword.getText().toString();
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "用户名和密码不能为空", Toast.LENGTH_SHORT).show();
+            } else if (username.equals("admin") && password.equals("123456")) {
+                Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "用户名或密码错误", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        new AlertDialog.Builder(this)
+                .setTitle("请输入账号密码")
+                .setIcon(R.mipmap.ic_launcher_round)
+                .setView(dialogView)
+                .setPositiveButton("登录", (dialog, which) -> {
+                    String username = etUsername.getText().toString();
+                    String password = etPassword.getText().toString();
+                    Toast.makeText(this, "用户名：" + username + '\n' + "密码：" + password, Toast.LENGTH_SHORT).show();
+
+                })
+                .setNegativeButton("取消", null)
+                .show();
+    }
 
 
 }
